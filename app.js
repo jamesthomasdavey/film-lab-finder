e8b7af48334ce8dea35d8b71629c17033b985b55
James Thomas Davey <james.t.davey@gmail.com>
2020-10-11 09:15:18 -0700

    project start

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+// import routes
+const userRoutes = require('./routes/user');
+
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('Connected to FLF database'));
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// routes middleware
+app.use('/api', userRoutes);
+
+// setting up port variable
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
+
+// handle db error
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`);
+});
