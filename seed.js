8b9953cf8c2fcc39fab2b2ca371e90bcf8cac10e
James Thomas Davey <james.t.davey@gmail.com>
2020-10-19 15:04:37 -0700

    seed all three categories

--- seed.js
+++ seed.js
@@ -1,50 +1,75 @@
+// import models
 const ServiceType = require('./models/serviceType');
+const FilmType = require('./models/filmType');
+const FilmSize = require('./models/filmSize');
 
-exports.seedPermanentData = () => {
-  // add 'Develop' service type
-  const develop = new ServiceType({
-    name: 'Develop',
-  });
-  develop.save((err, data) => {
+const serviceTypes = [
+  'Develop',
+  'Develop and scan',
+  'Develop, scan, and print',
+  'Scan',
+  'Scan and print',
+];
+
+const filmTypes = ['C-41', 'Black and White', 'E-6', 'ECN-2'];
+
+const filmSizes = [
+  '35mm',
+  '35mm Panoramic',
+  '35mm Mounted',
+  '120',
+  '220',
+  '4x5',
+  '8x10',
+];
+
+const seedServiceTypes = serviceTypes => {
+  if (serviceTypes.length === 0) return;
+  const newServiceType = new ServiceType({ name: serviceTypes[0] });
+  newServiceType.save((err, data) => {
     if (err) {
-      console.log(
-        `"Develop" already exists. Seeding is probably not necessary right now.`
-      );
+      console.log(`Service type "${serviceTypes[0]}" already exists.`);
     } else {
-      console.log('Added "Develop" service type.');
+      console.log(`Created service type "${serviceTypes[0]}".`);
     }
-    // add 'Develop and scan' service type
-    const developScan = new ServiceType({
-      name: 'Develop and scan',
-    });
-    developScan.save((err, data) => {
-      if (err) {
-        console.log(`Error: "Develop and scan" already exists.`);
-      } else {
-        console.log(`Added "Develop and scan" service type.`);
-      }
-      // add 'Develop, scan, and print' service type
-      const developScanPrint = new ServiceType({
-        name: 'Develop, scan, and print',
-      });
-      developScanPrint.save((err, data) => {
-        if (err) {
-          console.log(`Error: "Develop, scan, and print" already exists.`);
-        } else {
-          console.log(`Added "Develop, scan, and print" service type.`);
-        }
-        // add 'Scan' service type
-        const scan = new ServiceType({
-          name: 'Scan',
-        });
-        scan.save((err, data) => {
-          if (err) {
-            console.log(`Error: "Scan" already exists.`);
-          } else {
-            console.log(`Added "Scan" service type.`);
-          }
-        });
-      });
-    });
+    const myServiceTypes = [...serviceTypes];
+    myServiceTypes.shift();
+    seedServiceTypes(myServiceTypes);
   });
+};
+
+const seedFilmTypes = filmTypes => {
+  if (filmTypes.length === 0) return;
+  const newFilmType = new FilmType({ name: filmTypes[0] });
+  newFilmType.save((err, data) => {
+    if (err) {
+      console.log(`Film type "${filmTypes[0]}" already exists.`);
+    } else {
+      console.log(`Created film type "${filmTypes[0]}".`);
+    }
+    const myFilmTypes = [...filmTypes];
+    myFilmTypes.shift();
+    seedFilmTypes(myFilmTypes);
+  });
+};
+
+const seedFilmSizes = filmSizes => {
+  if (filmSizes.length === 0) return;
+  const newFilmSize = new FilmSize({ name: filmSizes[0] });
+  newFilmSize.save((err, data) => {
+    if (err) {
+      console.log(`Film size "${filmSizes[0]}" already exists.`);
+    } else {
+      console.log(`Created film size "${filmSizes[0]}".`);
+    }
+    const myFilmSizes = [...filmSizes];
+    myFilmSizes.shift();
+    seedFilmSizes(myFilmSizes);
+  });
+};
+
+exports.seedAll = () => {
+  seedServiceTypes(serviceTypes);
+  seedFilmTypes(filmTypes);
+  seedFilmSizes(filmSizes);
 };
