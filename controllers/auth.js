0b7ae043b591afa7a901ac7c0af8368958d5d679
James Thomas Davey <james.t.davey@gmail.com>
2020-10-21 13:31:02 -0700

    re-refactor earlier methods

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,12 +6,11 @@ const { errorHandler } = require('../helpers/dbErrorHandler');
 const cookieName = 'flf-token';
 
 exports.register = (req, res) => {
-  // console.log(`Request body: ${req.body}`);
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        error: errorHandler(err),
+        err: errorHandler(err),
       });
     }
     // prevent sending this data to user
@@ -26,33 +25,32 @@ exports.register = (req, res) => {
 exports.signin = (req, res) => {
   // find the user based on email
   const { email, password } = req.body;
-  User.findOne({ email: email })
-    .then(user => {
-      // if user is found, make sure the email and password match
-      if (!user.authenticate(password)) {
-        return res
-          .status(401)
-          .json({ error: 'Email and password do not match' });
-      }
-      // generate a signed token with user id and secret
-      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-      // persist the token as 't' in cookie with expiry date
-      res.cookie(cookieName, token, { expire: new Date() + 604800 });
-      // return response with user and token to frontend client
-      const { _id, name, email, role } = user;
-      return res.json({
-        token: token,
-        user: { _id, name, email, role },
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: 'User with that email does not exist',
       });
-    })
-    .catch(err =>
-      res.status(400).json({ error: 'User with that email does not exist' })
-    );
+    }
+    // if the user is found, make sure the email and password match
+    // create authenticate method in user model
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: 'Email and password do not match',
+      });
+    }
+    // generate a signed token with user id and secret
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    // persist the token as 't' in cookie with expiry date
+    res.cookie('t', token, { expire: new Date() + 604800 });
+    // return response with user and token to frontend client
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, email, name, role } });
+  });
 };
 
 exports.signout = (req, res) => {
-  res.clearCookie(cookieName);
-  res.json({ message: 'Signout successful' });
+  res.clearCookie('t');
+  res.json({ message: 'Signout success' });
 };
 
 ///////////////
