2a2fe192d724e09575de360bd5a323bc6af69a1c
James Thomas Davey <james.t.davey@gmail.com>
2020-10-28 21:40:20 -0700

    updated lab model to include settings

--- models/lab.js
+++ models/lab.js
@@ -9,18 +9,69 @@ const labSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
   },
-  servicesOffered: [
+  services: [
     {
       service: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Service',
       },
-      baseCost: {
+      doesOffer: {
+        type: Boolean,
+        default: false,
+      },
+      basePrice: {
         type: Number,
-        required: true,
+        required: doesOffer,
       },
     },
   ],
+  settings: {
+    rawByOrder: {
+      isEnabled: {
+        type: Boolean,
+        default: false,
+      },
+      price: {
+        type: Number,
+        required: isEnabled,
+      },
+    },
+    scanners: {
+      scannerA: String,
+      scannerB: String,
+      scannerC: String,
+    },
+    scanResolutions: {
+      f35mm: {
+        sizeA: Number,
+        sizeB: Number,
+        sizeC: Number,
+      },
+      f35mmPano: {
+        sizeA: Number,
+        sizeB: Number,
+        sizeC: Number,
+      },
+      f120: {
+        sizeA: Number,
+        sizeB: Number,
+        sizeC: Number,
+      },
+      f4x5: {
+        sizeA: Number,
+        sizeB: Number,
+        sizeC: Number,
+      },
+      f8x10: {
+        sizeA: Number,
+        sizeB: Number,
+        sizeC: Number,
+      },
+    },
+    customScanOptions: {
+      
+    }
+  },
 });
 
 module.exports = mongoose.model('Lab', labSchema);
